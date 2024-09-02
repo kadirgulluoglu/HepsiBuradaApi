66d415f4957b4516574578f1538b44ea7ab756f6
Kadir Güllüoğlu <kadirrbey001@gmail.com>
2024-09-02 13:59:15 +0300

    identity added

---
+++ Core/HepsiBuradaApi.Domain/Entities/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace HepsiBuradaApi.Domain.Entities
+{
+    public class Role : IdentityRole<Guid>
+    {
+        public Role()
+        {
+        }
+    }
+}
+
