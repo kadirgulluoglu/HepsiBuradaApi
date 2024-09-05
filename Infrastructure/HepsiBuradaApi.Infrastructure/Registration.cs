58b1594bf342a2a6ab6d8a6ad6c78622ad619767
Kadir Güllüoğlu <kadirrbey001@gmail.com>
2024-09-05 18:02:04 +0300

    token servisleri oluşturuldu

---
+++ Infrastructure/HepsiBuradaApi.Infrastructure/Registration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HepsiBuradaApi.Infrastructure.Tokens;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HepsiBuradaApi.Infrastructure
+{
+    public static class Registration
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<TokenSettings>(configuration.GetSection("JWT"));
+        }
+    }
+}
+
